a42633ba3c28cc0f0d67418dc42c3985c1f4f8ee
CornetS28 <corsam28@gmail.com>
2019-11-30 23:22:24 -0500

    more exercises on closures

--- FUNCTIONS/closures.js
+++ FUNCTIONS/closures.js
@@ -3,46 +3,94 @@ const createCounter = () => {
 
   return {
     increment() {
-      count++
+      count++;
     },
     decrement() {
-      count--
+      count--;
     },
     get() {
       return count;
-    }
-  }
-}
+    },
+  };
+};
 
 const counter = createCounter();
 
-counter.increment()
-counter.decrement()
-counter.increment()
+counter.increment();
+counter.decrement();
+counter.increment();
 
 console.log(counter.get());
 
-const adder = (a) => {
-  return (b) => {
-    return a + b
-  }
-}
+const adder = a => {
+  return b => {
+    return a + b;
+  };
+};
 
 const add5 = adder(5);
 
-console.log(add5(5))
-console.log(add5(-2))
+console.log(add5(5));
+console.log(add5(-2));
 
-const add30 = adder(30)
+const add30 = adder(30);
 console.log(add30(-20));
 
-const createTipper = (tipBase) => {
-  return (total)=> {
-    return total * tipBase
-  }
-}
+const createTipper = tipBase => {
+  return total => {
+    return total * tipBase;
+  };
+};
 
-const tip20 = createTipper(.2)
+const tip20 = createTipper(0.2);
+
+console.log(tip20(100));
+
+console.log('---------------');
+
+const tip = (defaultTip = 0.4) => {
+  return total => {
+    return total * defaultTip;
+  };
+};
+
+// const tip40 = tip(48)
+console.log(tip()(48));
+console.log('---------------');
+
+const averageGrade = (totalGrade = 0) => {
+  return numberOfCoursesTaken => {
+    return totalGrade / numberOfCoursesTaken;
+  };
+};
+
+const studentGrade = averageGrade(80);
+const result = studentGrade(5);
+console.log(result);
 
 
-console.log(tip20(100))
+console.log('------------------');
+
+const studentFinalGrade = () => {
+  let numberOfCoursesTaken = 0;
+  let totalNote = 0;
+
+  let result = 0;
+  return {
+    studentComptedNote(note) {
+      totalNote = note;
+    },
+    studentTotalCoursesTaken(courses) {
+      numberOfCoursesTaken = courses;
+    },
+    getFinalGrade() {
+      return (result = totalNote / numberOfCoursesTaken);
+    },
+  };
+};
+
+const studentOfficialGrade = studentFinalGrade();
+studentOfficialGrade.studentComptedNote(80);
+studentOfficialGrade.studentTotalCoursesTaken(5);
+
+console.log(studentOfficialGrade.getFinalGrade());
