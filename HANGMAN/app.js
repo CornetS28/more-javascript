e29fed535f720e6e4998369c4e2c5ee2946e318a
CornetS28 <corsam28@gmail.com>
2019-12-06 16:22:04 -0500

    using fetch

--- HANGMAN/app.js
+++ HANGMAN/app.js
@@ -1,15 +1,15 @@
-const guessEl = document.querySelector ('#guess');
-const h2 = document.createElement ('h2');
-const guessOutput = guessEl.appendChild (h2);
-const h4 = document.createElement ('h4');
-const remainingGuessesOutput = guessEl.appendChild (h4);
-const game1 = new Hangman ('My Cat', 2);
+const guessEl = document.querySelector("#guess");
+const h2 = document.createElement("h2");
+const guessOutput = guessEl.appendChild(h2);
+const h4 = document.createElement("h4");
+const remainingGuessesOutput = guessEl.appendChild(h4);
+const game1 = new Hangman("My Cat", 2);
 
 guessOutput.textContent = game1.puzzle;
 remainingGuessesOutput.textContent = game1.statusMessage;
 
-window.addEventListener ('keypress', e => {
-  if (game1.status === 'playing') {
+window.addEventListener("keypress", e => {
+  if (game1.status === "playing") {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuess(guess);
     guessOutput.textContent = game1.puzzle;
@@ -18,22 +18,30 @@ window.addEventListener ('keypress', e => {
   return;
 });
 
-
 getPuzzle('2').then((puzzle) => {
   console.log(puzzle)
-}, (err) => {
+}).catch((err) => {
   console.log(`Error: ${err}`)
-});
+})
 
-getCountry('HT').then((country) => {
-  console.log(`Country Name: ${country.name}`)
-}, (error) => {
-    console.log(`Error: ${error}`)
-});
+// getCountry('HT').then((country) => {
+//   console.log(`Country Name: ${country.name}`)
+// }, (error) => {
+//     console.log(`Error: ${error}`)
+// });
 
-
-
-
-
-
-
+// Fetch
+// fetch("http://puzzle.mead.io/puzzle", {})
+//   .then(response => {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error("Unable to fetch the puzzle");
+//     }
+//   })
+//   .then(data => {
+//     console.log(data.puzzle);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
--- HANGMAN/request.js
+++ HANGMAN/request.js
@@ -1,35 +1,32 @@
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-  const request = new XMLHttpRequest();
+const getPuzzle = wordCount => {
+ return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {})
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Cannot find puzzle");
+      }
+    }).then((data) => {
+      return data.puzzle 
+    })
+};
 
-  request.addEventListener('readystatechange', e => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      data = JSON.parse(e.target.responseText);
-      resolve(data.puzzle);
-    } else if (e.target.readyState === 4) {
-      reject('There was an error');
-    }
+const getCountry = countryCode =>
+  new Promise((resolve, reject) => {
+    const countryRequest = new XMLHttpRequest();
+
+    countryRequest.addEventListener("readystatechange", e => {
+      if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        const country = data.find(
+          country => country.alpha2Code === countryCode
+        );
+        resolve(country);
+      } else if (e.target.readyState === 4) {
+        reject("Unable to fetch data");
+      }
+    });
+
+    countryRequest.open("GET", "http://restcountries.eu/rest/v2/all");
+    countryRequest.send();
   });
-
-  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
-  request.send();
-})
-  
-
-
-const getCountry = (countryCode) => new Promise((resolve, reject) => {
-  const countryRequest = new XMLHttpRequest();
-
-  countryRequest.addEventListener('readystatechange', e => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.responseText);
-      const country = data.find(country => country.alpha2Code === countryCode);
-      resolve(country);
-    } else if (e.target.readyState === 4) {
-      reject('Unable to fetch data');
-    }
-  });
-
-  countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-  countryRequest.send();
-})
-
